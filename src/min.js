671f3c30f7bdbb59ad0afdbdd1d5ea54f538a4d3
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-31 23:34:46 +0900

    [ADD] 세 수 중 최소값 구하기

--- src/min.js
+++ src/min.js
@@ -1,1 +1,14 @@
 // 세 수 중 최소값
+
+function solution(a, b, c) {
+    let min;
+
+    if (a > b) min = b;
+    else min = a;
+
+    if (c < min) min = c;
+
+    return min;
+};
+
+console.log(solution(6, 5, 11));
